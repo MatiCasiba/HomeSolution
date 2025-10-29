1ad743dabb3b5149950e1ed3ca4edfe2a9d8dece
Matias Casiba <casibagabriel@gmail.com>
2025-10-29 15:11:25 -0300

    Agrego un método para validar las categorias

--- src/entidades/EmpleadoPlanta.java
+++ src/entidades/EmpleadoPlanta.java
@@ -8,8 +8,14 @@ public class EmpleadoPlanta extends Empleado {
 		super(legajo, nombre);
 		
 		if(valorDia <= 0) throw new IllegalArgumentException("El valor del día es mayor a 0");
-		
+		if(!categoriaValida(categoria)) throw new IllegalArgumentException("Categoría inválida, debe ser INICIAL, TECNICO o EXPERTO");
 		this.valorDia = valorDia;
 		this.categoria = categoria.toUpperCase();
+	}
+	
+	private boolean categoriaValida(String c) {
+		if(c == null) return false;
+		String cate = c.toUpperCase();
+		return cate.equals("INICIAL") || cate.equals("TECNICO") || cate.equals("EXPERTO");
 	}
 }
