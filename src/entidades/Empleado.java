b4b8504cb25653c1cf410ad4d878d0ad36f78b4c
Matias Casiba <casibagabriel@gmail.com>
2025-10-29 15:48:17 -0300

    (Corrección de error Empleado.java) Ahora la clase Empleado pasa a ser una clase abstracta

--- src/entidades/Empleado.java
+++ src/entidades/Empleado.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class Empleado {
+public abstract class Empleado {
 	protected int legajo;
 	protected String nombre;
 	protected boolean disponible;
