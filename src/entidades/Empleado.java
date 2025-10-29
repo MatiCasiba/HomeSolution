1745abfe7efd0aa19b146ec01a155388ec3128a0
Matias Casiba <casibagabriel@gmail.com>
2025-10-29 14:08:33 -0300

    Agrego los atributos y creo el constructor de la clase Empleado.java

--- src/entidades/Empleado.java
+++ src/entidades/Empleado.java
@@ -1,5 +1,22 @@
 package entidades;
 
-public class Empleado {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Empleado {
+	protected int legajo;
+	protected String nombre;
+	protected boolean disponible;
+	protected int retrasosTotales;
+	protected Map<Integer, Integer> retrasosPorProyecto;
+	
+	public Empleado(int legajo, String nombre) {
+		if(legajo<=0) throw new IllegalArgumentException("El legajo debe ser mayor que 0");
+		if(nombre == null || nombre.isBlank()) throw new IllegalArgumentException("El nombre no puede ser vacío");
+		this.legajo = legajo;
+		this.nombre = nombre;
+		this.disponible = true;
+		this.retrasosTotales = 0;
+		this.retrasosPorProyecto = new HashMap<>();
+	}
 }
