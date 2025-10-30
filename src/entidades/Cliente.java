2b7fef2c075a4567300982d41a4ab5349bde64e3
Matias Casiba <casibagabriel@gmail.com>
2025-10-30 16:01:00 -0300

    Armo el constructor de Cliente.java

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -5,4 +5,23 @@ public class Cliente {
 	private String telefono;
 	private String email;
 	
+	public Cliente(String nombre, String telefono, String email) {
+		
+		if(nombre == null || nombre.isBlank()) {
+			throw new IllegalArgumentException("El nombre no puede estar vacío");
+		}
+		if(telefono == null || telefono.isBlank()) {
+			throw new IllegalArgumentException("El teléfono no puede estar vacío");
+		}
+		if(email == null || email.isBlank()) {
+			throw new IllegalArgumentException("El email no puede estar vacío");
+		}
+		if(!email.contains("@") || !email.contains(".")) {
+			throw new IllegalArgumentException("El email debe tener '@' y '.' ppara ser válido ");
+		}
+		
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.email = email;
+	}
 }
