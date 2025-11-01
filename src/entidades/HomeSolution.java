1c75bea55eef7fa3bd80637bad853b9fb2baec38
Matias Casiba <casibagabriel@gmail.com>
2025-11-01 18:30:44 -0300

    Creo métodos para el registro de empleados

--- src/entidades/HomeSolution.java
+++ src/entidades/HomeSolution.java
@@ -11,6 +11,43 @@ public class HomeSolution implements IHomeSolution{
 	public HomeSolution() {
 		this.proyectos = new HashMap<>();
 		this.empleados = new HashMap<>();
+		this.contadorProyectos = 1;
+	}
+	
+	// REGISTRO DE EMPLEADOS
+	@Override
+	public void registrarEmpleado(String nombre, double valor) {
+		if (nombre == null || nombre.isBlank()) {
+			throw new IllegalArgumentException("El nombre no puede ser vacío");
+		}
+		if (valor <= 0) {
+			throw new IllegalArgumentException("El valor debe ser mayor a 0");
+		}
+
+		int legajo = generarLegajo();
+		EmpleadoContratado empleado = new EmpleadoContratado(legajo, nombre, valor);
+		empleados.put(legajo, empleado);
+	}
+	
+	@Override
+    public void registrarEmpleado(String nombre, double valor, String categoria) {
+        if (nombre == null || nombre.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede ser vacío");
+        }
+        if (valor <= 0) {
+            throw new IllegalArgumentException("El valor debe ser mayor a 0");
+        }
+        if (categoria == null || categoria.isBlank()) {
+            throw new IllegalArgumentException("La categoría no puede ser vacía");
+        }
+
+        int legajo = generarLegajo();
+        EmpleadoPlanta empleado = new EmpleadoPlanta(legajo, nombre, valor, categoria);
+        empleados.put(legajo, empleado);
+    }
+
+	private int generarLegajo() {
+		return empleados.keySet().stream().max(Integer::compareTo).orElse(0) + 1;
 	}
 	
 }
